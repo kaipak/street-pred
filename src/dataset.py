155cc7ab4c81a338e2c72460d0615a88904e7e5b
Kai Pak <kai@kaipak.org>
2019-11-28 18:01:56 -0800

    - Lyrics scrapper seems to work completely.

--- src/dataset.py
+++ src/dataset.py
@@ -5,23 +5,40 @@
 # https://www.kaggle.com/gyani95/380000-lyrics-from-metrolyrics
 # 
 
+from pandarallel import pandarallel
+import lyricsgenius
 import pandas as pd
 import numpy as np
 import re
 
 
-def process_datasets(af_csv, bb_csv, lyrics_csv):
+def process_datasets(af_csv, bb_csv, sample_size=None, filter='hip hop'):
 	"""Create consistent keys for identifying songs in corpus
+
 	"""
 
 	df_af = pd.read_csv(af_csv) 
+	df_af.dropna(inplace=True)
+	df_af = df_af.loc[df_af['artist_genre'].str.contains(filter)]
+
+	if sample_size:
+		df_af = df_af.sample(sample_size)
+
 	df_bb = pd.read_csv(bb_csv)
-	#df_ly = pd.read_csv(lyrics_csv)
 
-	df_af = process_songkey(df_af)
-	df_bb = process_songkey(df_bb)
+	# Subset dataframe and scrape song lyrics
+	pandarallel.initialize()
+	df_ly = df_bb[['songid', 'performer', 'song']].drop_duplicates()
+	df_ly = df_ly.loc[df_ly['songid'].isin(df_af['songid'])]
+	df_ly['lyrics'] = df_ly.parallel_apply(get_lyrics, axis=1)
 
-	return df_af, df_bb
+	return df_af, df_bb, df_ly
+
+
+def get_lyrics(row):
+	genius = lyricsgenius.Genius('QOkPNsgDXBxuex27C6TWN2R0EQJ5pol4baxhYde0rxlGoAZ2Hfyb3OpLXm52e8ta')
+	song = genius.search_song(row.performer, row.song)
+	return song.lyrics
 
 
 def process_songkey(df):
